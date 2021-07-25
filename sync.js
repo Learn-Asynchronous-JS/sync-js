429b44899f246d9c2b7bddc87ca0e9e108669ccc
Asitha Bandara <asitha@2c8.com>
2021-07-25 09:28:57 +0530

    Added sync code to demostrate call stack

---
+++ sync.js
@@ -0,0 +1,14 @@
+// 1 - JS starts from here
+console.log("Hi");
+
+//  2 - It stores the function in the memory
+function sayHelloTo(name) {
+    // 4 - prints the output
+    console.log(`Hello ${name} `);
+}
+
+// 3 - JS executes the function
+sayHelloTo("Asitha");
+
+// 5 - Ends here
+console.log("Bye");
